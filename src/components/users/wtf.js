244c8ba2313035c6d84fd53e358577dbb0d32a8d
JennBlunt <jennbluntphoto@gmail.com>
2020-03-27 15:22:36 -0400

    added user grid, hardcoded

---
+++ src/components/users/wtf.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class wtf extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default wtf
