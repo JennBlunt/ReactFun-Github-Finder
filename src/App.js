6c026fbc4e10e02bd7c8d2cc6ec1886d30647c5e
JennBlunt <jennbluntphoto@gmail.com>
2020-03-28 17:00:34 -0400

    set up Router

--- src/App.js
+++ src/App.js
@@ -1,52 +1,72 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import axios from 'axios';
 import Navbar from '../src/components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
+import About from './components/pages/About';
 import './App.css';
 
 class App extends Component {
   state = {
     users: [],
-    loading: false
-  }
+    loading: false,
+    alert: null
+  };
 
   async componentDidMount() {
     this.setState({ loading: true });
     const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&cliet_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
     this.setState({ users: res.data, loading: false });
-  }
+  };
 
   // search github users
   searchUsers = async text => {
     this.setState({ loading: true });
     const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&cliet_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
     this.setState({ users: res.data.items, loading: false });
-  }
+  };
 
   //clear users from state
   clearUsers = () => {
     this.setState({ users: [], loading: false });
-  }
+  };
+
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+    setTimeout(() => this.setState({ alert: null }), 3000);
+  };
 
 
   render() {
     const { users, loading } = this.state;
     return (
-      <div className="App">
-        < Navbar />
-        <div className='container'>
-          <Search
-            searchUsers={this.searchUsers}
-            clearUsers={this.clearUsers}
-            showClear={users.length > 0 ? true : false}
-          />
-          <Users
-            loading={loading}
-            users={users}
-          />
+      <Router>
+        <div className="App">
+          < Navbar />
+          <div className='container'>
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route exact path='/' render={props => (
+                <Fragment>
+                  <Search
+                    searchUsers={this.searchUsers}
+                    clearUsers={this.clearUsers}
+                    showClear={users.length > 0 ? true : false}
+                    setAlert={this.setAlert}
+                  />
+                  <Users
+                    loading={loading}
+                    users={users}
+                  />
+                </Fragment>
+              )} />
+              <Route exact path='/about' component={About} />
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -9,14 +9,18 @@ class Search extends Component {
     static propTypes = {
         searchUsers: PropTypes.func.isRequired,
         clearUsers: PropTypes.func.isRequired,
-        showClear: PropTypes.bool.isRequired
+        showClear: PropTypes.bool.isRequired,
+        searchAlert: PropTypes.func.isRequired
     }
 
     onSubmit = (e) => {
         e.preventDefault();
-        this.props.searchUsers(this.state.text);
-        this.setState({ text: '' });
-
+        if (this.state.text === '') {
+            this.props.setAlert('Please enter a user name to search', 'light');
+        } else {
+            this.props.searchUsers(this.state.text);
+            this.setState({ text: '' });
+        }
     }
     onChange = (e) => this.setState({ text: e.target.value });
 
